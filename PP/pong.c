608b2f7bc29167b8b7a4ba090f73ba7d88dba5ce
Will Osborne <w.osborne@warwick.ac.uk>
2016-11-29 15:00:50 +0000

    POONG

--- PP/pong.c
+++ PP/pong.c
@@ -1,3 +1,4 @@
+//#include <stdlib.h>
 typedef volatile unsigned int ioreg;
 
 // register containing which pins we want to enable
@@ -76,8 +77,14 @@ void waitForSPI(void);
 void setX(int);
 void setY(int);
 
+void drawFirework(int, int);
+
+void delay(int);
 int getInputA(void);
 int getInputB(void);
+int adjustInput(int);
+void resetBall(int);
+int rand(void);
 
 void drawLine(int, int, int, int);
 void drawVLine(int, int, int);
@@ -96,6 +103,24 @@ int ballvY = 2;
 int player1Input = 0;
 int player2Input = 0;
 int paddleSize = 100;
+int player1Score = 0;
+int player2Score = 0;
+int seed = 3;
+
+int fireworksFrame = 0;
+int fireworkCounter = 0;
+int fireworkFinished = 0;
+int accumulator = 0;
+int frameDelay = 0x130000;
+
+
+int state;
+
+const int GAME = 0;
+const int FIREWORKS = 1;
+
+void drawChar (int, int, char);
+void changeState (int);
 
 int main(void)
 {
@@ -155,7 +180,7 @@ int main(void)
     //*PIO_OER = OUTPUTS; // Enable output driver for pin
     //*PIO_IFER = INPUTS; // Turn on filter for inputs
 
-    
+
     //int WRITE_B = 0x4000; //0b0100000000000000;
     //int WRITE_A = 0xc000; //0b1100000000000000;
     //*SPI_TDR = WRITE_TO_B_AND_BUFFER; // write 32 to buffer and B
@@ -165,77 +190,475 @@ int main(void)
 
     //waitForSPI();
 
+    seed = getInputA() * getInputB();
+    state = GAME;
+
     while(1) {
-        //draw bounding box
-        register int i = 0;
-        setX(50);
-        for (i = 0; i < 1023; i++) {
-            setY(i);
+        if (state == GAME) {
+            //draw bounding box
+            register int i = 0;
+
+            /* setX(50); */
+            /* for (i = 0; i < 1023; i++) { */
+            /*     setY(i); */
+            /* } */
+            /* setX(950); */
+            /* for (i = 0; i < 1023; i++) { */
+            /*     setY(i); */
+            /* } */
+            /* setY(50); */
+            /* for (i = 0; i < 1023; i++) { */
+            /*     setX(i); */
+            /* } */
+            /* setY(950); */
+            /* for (i = 0; i < 1023; i++) { */
+            /*     setX(i); */
+            /* } */
+
+            drawVLine(50, 50, 950);
+            drawVLine(950, 50, 950);
+
+            drawHLine(50, 50, 950);
+            drawHLine(950, 50, 950);
+            //get input from players
+            player1Input = getInputA();
+            player1Input = adjustInput(player1Input);
+
+            player1Input -= 190;
+
+            player2Input = getInputB();
+            player2Input = adjustInput(player2Input);
+
+            player2Input -= 190;
+            if (player1Input + paddleSize > 950)
+                player1Input = 950 - paddleSize;
+            if (player2Input + paddleSize > 950)
+                player2Input = 950 - paddleSize;
+
+            if (player1Input < 50)
+                player1Input = 50;
+
+            if (player2Input < 50)
+                player2Input = 50;
+
+            drawVLine(75, player1Input, player1Input + paddleSize);
+            drawVLine(925, player2Input, player2Input + paddleSize);
+
+
+
+            //draw paddles
+
+            //setX(75);
+
+
+            /* drawLine(100, 200, 700, 900); */
+            //setY()
+
+
+
+            // move ball by velocity
+            ballX += ballvX;
+            ballY += ballvY;
+
+            if (ballY - player1Input <= paddleSize && ballY > player1Input) {
+                if (ballX < 75 && ballX > 50){
+                    ballvX = -ballvX;
+                    ballX = 75;
+                }
+            }
+            if (ballY - player2Input <= paddleSize && ballY > player2Input) {
+                if (ballX > 925 && ballX < 950){
+                    ballvX = -ballvX;
+                    ballX = 925;
+                }
+            }
+
+            if(ballY > 950 || ballY < 50)
+                ballvY = -ballvY;
+            if(ballX > 950) {
+                resetBall(1);
+                player1Score++;
+            }
+            if(ballX < 50) {
+                resetBall(-1);
+                player2Score++;
+            }
+
+            if (player1Score > 9) 
+                changeState(FIREWORKS);
+            if (player2Score > 9)
+                changeState(FIREWORKS);
+
+            //y *= 2;
+            drawChar(300, 700, '0'+player1Score);
+            drawChar(600, 700, '0'+player2Score);
+
+            setX(ballX);
+            setY(ballY);
+
+
+
+            delay(0x8000);
+
+            /* if ((*PIO_PDSR & RED_SWITCH) != RED_SWITCH) { */
+            /*     continue; */
+            /* } */    
         }
-        setX(950);
-        for (i = 0; i < 1023; i++) {
-            setY(i);
+
+        else if (state == FIREWORKS) {
+            
+            if (player1Score > player2Score){
+                drawChar(200, 300, 'p');
+                drawChar(250, 300, '1');
+                drawChar(450, 300, 'w');
+                drawChar(530, 300, '1');
+                drawChar(650, 300, 'n');
+                drawChar(750, 300, '5');
+            }
+            if (player2Score > player1Score){
+                drawChar(200, 300, 'p');
+                drawChar(250, 300, '1');
+                drawChar(450, 300, 'w');
+                drawChar(530, 300, '1');
+                drawChar(650, 300, 'n');
+                drawChar(750, 300, '5');
+            }
+
+            if (fireworkCounter == 0)
+                drawFirework(512, 512);
+            else if (fireworkCounter == 1)
+                drawFirework(200, 300);
+            else if (fireworkCounter == 2)
+                drawFirework(800, 600);
+            else if (fireworkCounter == 3)
+                drawFirework(300, 800);
+            else if (fireworkCounter == 4)
+                drawFirework(400, 600);
+            else if (fireworkCounter == 5)
+                drawFirework(700, 200);
+            ///fireworkCounter++;
+            if (fireworkCounter > 5)
+                changeState(GAME);
         }
-        setY(50);
-        for (i = 0; i < 1023; i++) {
-            setX(i);
-        }
-        setY(950);
-        for (i = 0; i < 1023; i++) {
-            setX(i);
-        }
+        // 0xd### writes the bits contained in ### to the control register in fast mode.
 
-        //get input from players
-        player1Input = getInputA();
-        player2Input = getInputB();
+        //int INPUTS = 0;
 
-        //draw paddles
+        //*PIO_PER = RED_LED; // Enable control of I/O pin from PIO Controller
+        //*PIO_OER = RED_LED; // Enable output driver for pin
+        /* *PIO_PER = OUTPUTS|INPUTS; // Enable control of I/O pin from PIO Controller */
+        //*PIO_OER = OUTPUTS; // Enable output driver for pin
+        /* *PIO_IFER = INPUTS; // Turn on filter for inputs */
+        //*PMC_PCER = PIO_IDENTIFIER; //enable the clock (regularly check if the inputs have changed -i.e. enable inputs)
 
-        setX(75);
-        //setY()
-        
-        
+        //*PIO_CODR = OUTPUTS; //clear all outputs
 
-        // move ball by velocity
-        ballX += ballvX;
-        ballY += ballvY;
+        //int PIN_6 = (1 << 5); // take 00000001, shift it left 5 times -> 0100000
 
-        if(ballY > 950 || ballY < 50)
-            ballvY = -ballvY;
-        if(ballX > 950)
-            ballvX = -ballvX;
-        if(ballX < 50)
-            ballvX = -ballvX;
+    }
+}
 
+void drawFirework(int x, int y){
 
+    int width = 150;
+    int diagonal = 110;
 
-        //y *= 2;
-        
-        setX(ballX);
-        setY(ballY);
+    int length = 100;
 
+    if (fireworksFrame == 0) {
+        width = 50;
+        diagonal = 30;
+        length = 50;
+        drawLine(x + width, y, x + width + length, y);
+        drawLine(x - width, y, x - width - length, y);
+
+        drawLine(x, y + width, x, y + width + length);            
+        drawLine(x, y - width, x, y - width - length);
+
+        drawLine(x + diagonal, y + diagonal, x + diagonal + diagonal, y + diagonal +  diagonal);
+        drawLine(x - diagonal, y - diagonal, x - diagonal - diagonal, y - diagonal -  diagonal);
+        drawLine(x + diagonal, y - diagonal, x + diagonal + diagonal, y - diagonal -  diagonal);
+        drawLine(x - diagonal, y + diagonal, x - diagonal - diagonal, y + diagonal +  diagonal);
         delay(0x8000);
 
-        /* if ((*PIO_PDSR & RED_SWITCH) != RED_SWITCH) { */
-        /*     continue; */
-        /* } */    
     }
-    // 0xd### writes the bits contained in ### to the control register in fast mode.
+    else if (fireworksFrame == 1) {
+        width = 70;
+        diagonal = 50;
+        length = 70;
+        drawLine(x + width, y, x + width + length, y);
+        drawLine(x - width, y, x - width - length, y);
 
-    //int INPUTS = 0;
+        drawLine(x, y + width, x, y + width + length);            
+        drawLine(x, y - width, x, y - width - length);
 
-    //*PIO_PER = RED_LED; // Enable control of I/O pin from PIO Controller
-    //*PIO_OER = RED_LED; // Enable output driver for pin
-    /* *PIO_PER = OUTPUTS|INPUTS; // Enable control of I/O pin from PIO Controller */
-    //*PIO_OER = OUTPUTS; // Enable output driver for pin
-    /* *PIO_IFER = INPUTS; // Turn on filter for inputs */
-    //*PMC_PCER = PIO_IDENTIFIER; //enable the clock (regularly check if the inputs have changed -i.e. enable inputs)
+        drawLine(x + diagonal, y + diagonal, x + diagonal + diagonal, y + diagonal +  diagonal);
+        drawLine(x - diagonal, y - diagonal, x - diagonal - diagonal, y - diagonal -  diagonal);
+        drawLine(x + diagonal, y - diagonal, x + diagonal + diagonal, y - diagonal -  diagonal);
+        drawLine(x - diagonal, y + diagonal, x - diagonal - diagonal, y + diagonal +  diagonal);
+        delay(0x8000);
+    }
+    else if (fireworksFrame == 2) {
+        width = 100;
+        diagonal = 70;
+        length = 100;
+        int tweak = 0;
+        drawLine(x + width, y, x + width + length, y);
+        drawLine(x - width, y, x - width - length, y);
 
-    //*PIO_CODR = OUTPUTS; //clear all outputs
+        drawLine(x, y + width, x, y + width + length);            
+        drawLine(x, y - width, x, y - width - length);
 
-    //int PIN_6 = (1 << 5); // take 00000001, shift it left 5 times -> 0100000
+        drawLine(x + diagonal, y + diagonal, x + diagonal + diagonal-tweak, y + diagonal +  diagonal-tweak);
+        drawLine(x - diagonal, y - diagonal, x - diagonal - diagonal+tweak, y - diagonal -  diagonal+tweak);
+        drawLine(x + diagonal, y - diagonal, x + diagonal + diagonal-tweak, y - diagonal -  diagonal+tweak);
+        drawLine(x - diagonal, y + diagonal, x - diagonal - diagonal+tweak, y + diagonal +  diagonal-tweak);
+        delay(0x8000);
+    }
+    accumulator += 0x8000;
+    if (accumulator > frameDelay) {
+        fireworksFrame++;
+        accumulator = 0;
+    }
+    if (fireworksFrame > 2) {
+        fireworksFrame = 0;
+
+        fireworkCounter++;
+    }
 
 }
+
+void changeState(int newState) {
+    state = newState;
+    if (newState == FIREWORKS) {
+        fireworksFrame = 0;
+        accumulator = 0;
+        fireworkCounter = 0;
+        fireworkFinished = 0;
+    }
+    if (newState == GAME){
+        resetBall(1);
+        player1Score = 0;
+        player2Score = 0;
+    }
+}
+
+int rand() {
+    seed = (2 * seed + 3) % 10;
+    return seed;
+}
+
+void resetBall(int player) {
+    ballX = 512;
+    ballY = 512;
+    ballvX = 5 * player;
+    ballvY = rand() - 5;
+    while (ballvY == 0)
+        ballvY = rand() - 5;
+    //delay(0x90000);
+}
+
+/// {{{
+void drawChar (int x, int y, char toDraw) {
+    int height = 100;
+    int width = 80;
+    switch (toDraw) {
+        case '0':
+            //drawLine(x, y, x, y+height);
+            //drawLine(x, y+height, x+width, y+height);
+            //drawLine(x+width, y, x+width, y+height);
+            //drawLine(x, y, x+width, y);
+            //
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            //drawHLine(y+height/2, x, x+width);
+            break;
+        case '1':
+            //drawVLine(x, y, y+height/2);
+            //drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            //drawHLine(y, x, x+width/2);
+            //drawHLine(y, x+width/2, x+width);
+
+            //drawHLine(y+height, x, x+width/2);
+            //drawHLine(y+height, x+width/2, x+width);
+
+            //drawHLine(y+height/2, x, x+width);
+            break;
+        case '2':
+            drawVLine(x, y, y+height/2);
+            //drawVLine(x, y+height/2, y+height);
+
+            //drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+            //drawHLine(y, x+width/2, x+width);
+
+            drawHLine(y+height, x, x+width);
+            //drawHLine(y+height, x+width/2, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '3':
+            //drawVLine(x, y, y+height/2);
+            //drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+            //drawHLine(y, x+width/2, x+width);
+
+            drawHLine(y+height, x, x+width);
+            //drawHLine(y+height, x+width/2, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '4':
+            //drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            //drawHLine(y, x, x+width/2);
+            //drawHLine(y, x+width/2, x+width);
+
+            //drawHLine(y+height, x, x+width);
+            //drawHLine(y+height, x+width/2, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '5':
+            //drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            //drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '6':
+
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            //drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '7':
+
+            //drawVLine(x, y, y+height/2);
+            //drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            //drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            //drawHLine(y+height/2, x, x+width);
+            break;
+        case '8':
+
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case '9':
+
+            //drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case 'p':
+
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            //drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+
+            //drawHLine(y, x, x+width);
+
+            drawHLine(y+height, x, x+width);
+
+            drawHLine(y+height/2, x, x+width);
+            break;
+        case 'w':
+
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width*1.5, y, y+height/2);
+            drawVLine(x+width*1.5, y+height/2, y+height);
+            
+            drawLine(x, y, x+width*0.75, y+height);
+
+            drawLine(x+width*0.75, y+height, x+width*1.5, y);
+            //drawHLine(y, x, x+width);
+
+            //drawHLine(y+height, x, x+width);
+
+            //drawHLine(y+height/2, x, x+width);
+            break;
+        case 'n':
+
+            drawVLine(x, y, y+height/2);
+            drawVLine(x, y+height/2, y+height);
+
+            drawVLine(x+width, y, y+height/2);
+            drawVLine(x+width, y+height/2, y+height);
+            
+            drawLine(x, y+height, x+width, y);
+            //drawHLine(y, x, x+width);
+
+            //drawHLine(y+height, x, x+width);
+
+            //drawHLine(y+height/2, x, x+width);
+            break;
+    }
+}
+// }}}
 
 int getInputA() {
     *ADC_CR = 0x2;
@@ -249,16 +672,27 @@ int getInputB() {
     return *ADC_CDR5;
 }
 
+int adjustInput(int input) {
+    int val = input * 2;
+    if (val > 1023)
+        val = 1022;
+    if (val < 0)
+        val = 0;
+    return val;
+}
+
 //RANGE IS 0-1023
 void setY(int y) {
     *SPI_TDR = WRITE_A | (y << 2);
     waitForSPI();
+    //delay(0x0001);
 }
 
 //RANGE IS 0-1023
 void setX(int x) {
     *SPI_TDR = WRITE_B | (x << 2);
     waitForSPI();
+    //delay(0x0001);
 }
 
 void drawVLine(int x, int startY, int endY) {
@@ -278,18 +712,18 @@ void drawHLine(int y, int startX, int endX) {
 }
 
 void drawLine(int startX, int startY, int endX, int endY) {
-    float m = (endY - startY) / (endX - startX);
+    float dX = endX - startX;
+    float dY = endY - startY;
 
     int interval = 100;
-    float intervalX = (endX - startX) / interval;
 
-    float y = startY;
-    register int x = 0;
-    for (x = startX; x < endX; x += intervalX) {
-        setX(x);
-        setY(y);
+    dX /= interval;
+    dY /= interval;
 
-        y += m * intervalX;
+    register int i = 0;
+    for (i = 0; i <= interval; i++) {
+        setX(startX + (int)(i * dX));
+        setY(startY + (int)(i * dY));
     }
 }
 
